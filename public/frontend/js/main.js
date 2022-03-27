e112a8f81850152d44c75197a2e54863d55a7bf3
WiIIiam <us.thanhlong18@gmail.com>
2022-03-27 12:30:23 +0700

    edit getElêmntById logo

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -132,9 +132,9 @@
 
         $(".tf-text").removeClass("style")
         $(".tf-text").addClass("s1")
-        document.getElementById("logo_header").src = "{{ asset('public/frontend/images/logo/logo_dark.png') }}";
-        document.getElementById("logo_footer").src = "{{ asset('public/frontend/images/logo/logo_dark.png') }}";
-        document.getElementById("moon_dark").src = "{{ asset('public/frontend/images/icon/moon-2.png') }}";
+        document.getElementById("logo_header").src = "public/frontend/images/logo/logo_dark.png";
+        document.getElementById("logo_footer").src = "public/frontend/images/logo/logo_dark.png";
+        document.getElementById("moon_dark").src = "public/frontend/images/icon/moon-2.png";
     });
 
     $(".light").on('click', function (e) {
@@ -145,9 +145,9 @@
 
         $(".tf-text").addClass("style")
         $(".tf-text").removeClass("s1")
-        document.getElementById("logo_header").src = "{{ asset('public/frontend/images/logo/logo.png') }}";
-        document.getElementById("logo_footer").src = "{{ asset('public/frontend/images/logo/logo2.png') }}";
-        document.getElementById("moon_dark").src = "{{ asset('public/frontend/images/icon/moon.png') }}";
+        document.getElementById("logo_header").src = "public/frontend/images/logo/logo.png";
+        document.getElementById("logo_footer").src = "public/frontend/images/logo/logo2.png";
+        document.getElementById("moon_dark").src = "public/frontend/images/icon/moon.png";
     });
 
     // Header Connect
