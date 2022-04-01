c85edb43b0c7522645e5d3a4dc7918c1f5dceb7e
WiIIiam <us.thanhlong18@gmail.com>
2022-04-02 01:50:08 +0700

    update UI

--- resources/views/client/sub/cart/checkout.blade.php
+++ resources/views/client/sub/cart/checkout.blade.php
@@ -21,13 +21,13 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-title-heading mg-bt-12">
-                        <h1 class="heading text-center">Check Out</h1>
+                        <h1 class="heading text-center">Checkout</h1>
                     </div>
                     <div class="breadcrumbs style2">
                         <ul>
                             <li><a href="/">Home</a></li>
                             <li><a href="#">Pages</a></li>
-                            <li>Create Item</li>
+                            <li>Checkout</li>
                         </ul>
                     </div>
                 </div>
